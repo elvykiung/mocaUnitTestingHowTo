d11028168a6fb6f4b878dd7288650097987a1396
Elvy_kiung <kiungh22@gmail.com>
2019-10-08 11:55:41 -0700

    add test code on operation function

---
+++ operation.js
@@ -0,0 +1,7 @@
+//function for add operator
+
+const add = (x, y) => +x + +y;
+const subtract = (x, y) => +x - +y;
+const multiply = (x, y) => +x * +y;
+const divide = (x, y) => +x / +y;
+module.exports = { add, subtract, multiply, divide };
